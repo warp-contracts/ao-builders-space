31e702f78ccca06a7b5c08be3824d5b282fb6eea
Asia <ziola.jm@gmail.com>
2024-04-22 16:55:18 +0200

    second try

--- wp-includes/js/dist/url.js
+++ wp-includes/js/dist/url.js
@@ -692,7 +692,7 @@ function isValidProtocol(protocol) {
  * @example
  * ```js
  * const authority1 = getAuthority( 'https://wordpress.org/help/' ); // 'wordpress.org'
- * const authority2 = getAuthority( 'http://ao.local:8080/test/' ); // 'localhost:8080'
+ * const authority2 = getAuthority( 'https://ao-builders-space.vercel.app:8080/test/' ); // 'localhost:8080'
  * ```
  *
  * @return {string|void} The authority part of the URL.
@@ -733,7 +733,7 @@ function isValidAuthority(authority) {
  *
  * @example
  * ```js
- * const path1 = getPath( 'http://ao.local:8080/this/is/a/test?query=true' ); // 'this/is/a/test'
+ * const path1 = getPath( 'https://ao-builders-space.vercel.app:8080/this/is/a/test?query=true' ); // 'this/is/a/test'
  * const path2 = getPath( 'https://wordpress.org/help/faq/' ); // 'help/faq'
  * ```
  *
@@ -775,7 +775,7 @@ function isValidPath(path) {
  *
  * @example
  * ```js
- * const queryString = getQueryString( 'http://ao.local:8080/this/is/a/test?query=true#fragment' ); // 'query=true'
+ * const queryString = getQueryString( 'https://ao-builders-space.vercel.app:8080/this/is/a/test?query=true#fragment' ); // 'query=true'
  * ```
  *
  * @return {string|void} The query string part of the URL.
@@ -881,7 +881,7 @@ function isValidQueryString(queryString) {
  *
  * @example
  * ```js
- * const pathAndQueryString1 = getPathAndQueryString( 'http://ao.local:8080/this/is/a/test?query=true' ); // '/this/is/a/test?query=true'
+ * const pathAndQueryString1 = getPathAndQueryString( 'https://ao-builders-space.vercel.app:8080/this/is/a/test?query=true' ); // '/this/is/a/test?query=true'
  * const pathAndQueryString2 = getPathAndQueryString( 'https://wordpress.org/help/faq/' ); // '/help/faq'
  * ```
  *
@@ -904,7 +904,7 @@ function getPathAndQueryString(url) {
  *
  * @example
  * ```js
- * const fragment1 = getFragment( 'http://ao.local:8080/this/is/a/test?query=true#fragment' ); // '#fragment'
+ * const fragment1 = getFragment( 'https://ao-builders-space.vercel.app:8080/this/is/a/test?query=true#fragment' ); // '#fragment'
  * const fragment2 = getFragment( 'https://wordpress.org#another-fragment?query=true' ); // '#another-fragment'
  * ```
  *
@@ -1318,7 +1318,7 @@ function cleanForSlug(string) {
  *
  * @example
  * ```js
- * const filename1 = getFilename( 'http://ao.local:8080/this/is/a/test.jpg' ); // 'test.jpg'
+ * const filename1 = getFilename( 'https://ao-builders-space.vercel.app:8080/this/is/a/test.jpg' ); // 'test.jpg'
  * const filename2 = getFilename( '/this/is/a/test.png' ); // 'test.png'
  * ```
  *
